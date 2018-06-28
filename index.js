b3dec341d4d2763c9f4632ab5da815c76277b124
Michael Ballard <ballard.michael9@gmail.com>
2018-06-28 14:04:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ function Scooter(year, color, model ){
   this.year = year
   this.color = color
   this.model = model
-  
+}
+
+function Driver (name,age, experience){
+  this.name = name 
+  this.color = color
+  this.model = model
 }
